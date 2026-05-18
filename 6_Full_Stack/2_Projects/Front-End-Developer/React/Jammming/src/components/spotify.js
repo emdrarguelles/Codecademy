4ad0a0dd831cd80b4be6c077678beb4a8a53d018
Ed Marcus Arguelles <edmarcus.arguelles@gmail.com>
2026-05-18 15:17:07 +0800

    feat: update redirecturi for netlify and local

--- 6_Full_Stack/2_Projects/Front-End-Developer/React/Jammming/src/components/spotify.js
+++ 6_Full_Stack/2_Projects/Front-End-Developer/React/Jammming/src/components/spotify.js
@@ -1,5 +1,5 @@
 const clientId = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
-const redirectUri = 'https://emdrargon-jammming.netlify.app';
+const redirectUri = import.meta.env.VITE_REDIRECT_URI || 'http://127.0.0.1:5173';
 
 let accessToken;
 

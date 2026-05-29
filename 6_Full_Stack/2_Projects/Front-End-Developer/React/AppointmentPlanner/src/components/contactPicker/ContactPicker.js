e72046c006c98caba5a5cc94721e269d94befa20
Ed Marcus Arguelles <edmarcus.arguelles@gmail.com>
2026-05-29 14:04:17 +0800

    feat: implement ContactPicker with dynamic contact options

--- 6_Full_Stack/2_Projects/Front-End-Developer/React/AppointmentPlanner/src/components/contactPicker/ContactPicker.js
+++ 6_Full_Stack/2_Projects/Front-End-Developer/React/AppointmentPlanner/src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,15 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({ contacts, value, onChange, name="contact" }) => {
   return (
-    <></>
+    <>
+      <label htmlFor={name}>Choose a contact:</label>
+      <select name={name} id={name} onChange={onChange} value={value}>
+        <option value="">No Contact Selected</option>
+        {contacts.map((obj, index) => (
+          <option key={index} value={obj.name}>{obj.name}</option>
+        ))}
+      </select>
+    </>
   );
 };

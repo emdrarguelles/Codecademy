0b6b420737ccf04e88612f958f3674d74d55bae4
Ed Marcus Arguelles <edmarcus.arguelles@gmail.com>
2026-06-08 16:54:23 +0800

    feat: add PHP currency and update conversion

--- 6_Full_Stack/2_Projects/Front-End-Developer/React/2 - Redux/CodecademyStore/src/utilities/utilities.js
+++ 6_Full_Stack/2_Projects/Front-End-Developer/React/2 - Redux/CodecademyStore/src/utilities/utilities.js
@@ -4,6 +4,8 @@ export function calculatePrice(price, currency) {
         return price * 0.86;
       case 'CAD':
         return price * 1.33;
+      case 'PHP':
+        return price * 55.00;
       default:
         return price;
     }
@@ -25,6 +27,8 @@ export function calculatePrice(price, currency) {
         return '€';
       case 'CAD':
         return '$';
+      case 'PHP':
+        return '₱';
       default:
         return '';
     }
